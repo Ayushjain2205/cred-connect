a0459c32da3de546cf24de38be57ec1eda9d1d1a
Ayush Jain <ayushjain2205@gmail.com>
2023-10-03 00:09:36 +0530

    Make table component reactive

--- src/components/base/Table.js
+++ src/components/base/Table.js
@@ -1,38 +1,36 @@
 import React from "react";
 
 const Table = () => {
+  const headers = ["Position", "Name", "Job", "Favorite Color"];
+  const rows = [
+    ["1", "Cy Ganderton", "Quality Control Specialist", "Blue"],
+    ["1", "Hart Hagerty", "Desktop Support Technician", "Purple"],
+    ["1", "Brice Swyre", "Tax Accountant", "Red"],
+  ];
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table w-1/2 border-collapse rounded-[10px]">
-        <thead>
-          <tr className="border-2 border-black rounded-[10px]">
-            <th className="font-medium">Position</th>
-            <th className="font-medium">Name</th>
-            <th className="font-medium">Job</th>
-            <th className="font-medium">Favorite Color</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="border-2 border-black">
-            <td>1</td>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
-          </tr>
-          <tr className="border-2 border-black">
-            <td>1</td>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-          <tr className="border-2 border-black">
-            <td>1</td>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="overflow-x-auto border-2 border-black rounded-[10px]">
+      <div className="flex border-b-2 border-black">
+        {headers.map((header) => (
+          <div key={header} className="flex-1 font-medium p-4">
+            {header}
+          </div>
+        ))}
+      </div>
+      {rows.map((row, rowIndex) => (
+        <div
+          key={rowIndex}
+          className={`flex font-bold hover:bg-[#FF84C2] ${
+            rowIndex !== rows.length - 1 ? "border-b-2 border-black" : ""
+          }`}
+        >
+          {row.map((cell, cellIndex) => (
+            <div key={cellIndex} className="flex-1 p-4">
+              {cell}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
