35039e595e189736a7eed1627259eaef522258c7
Ayush Jain <ayushjain2205@gmail.com>
2023-10-03 15:16:20 +0530

    Init thirdweb

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { ConnectWallet } from "@thirdweb-dev/react";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row items-center h-[80px] px-[20px] border-b-2 border-black">
-      <div className="flex flex-row gap-[10px]">
-        <img src="/icons/logo.svg" className="h-[48px]" alt="" />
+    <div className="flex flex-row items-center justify-between h-[80px] px-[20px] border-b-2 border-black">
+      {/* Logo and name */}
+      <div className="flex flex-row gap-[10px] items-center">
+        <img src="/icons/logo.svg" className="h-[48px]" alt="logo" />
         <span className="font-medium text-[32px]">CredConnect</span>
+      </div>
+
+      {/* Navigation items */}
+      <div className="flex flex-grow justify-center space-x-[15px]">
+        <a href="/" className="text-lg font-medium">
+          Home
+        </a>
+        <a href="/about" className="text-lg font-medium">
+          About
+        </a>
+        <a href="/contact" className="text-lg font-medium">
+          Contact
+        </a>
+      </div>
+
+      {/* Button on the right */}
+      <div>
+        <ConnectWallet />
       </div>
     </div>
   );
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import Layout from "../components/UI/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThirdwebProvider
+      activeChain="ethereum"
+      clientId="c2c1ec3b031f50949b98ed6a07730dc5"
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThirdwebProvider>
   );
 }
